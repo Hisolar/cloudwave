1727cf2d1668d0e87626e23a9e86ef1736887c84
Babatunde Wahab Ishola <Solar@SolarMac.local>
2023-09-17 11:45:06 +0100

    working on use section

--- src/Component/Use.jsx
+++ src/Component/Use.jsx
@@ -1,4 +1,5 @@
 import dest from "../assets/dest.png";
+import deckimage from "../assets/deckimage.png";
 import next from "../assets/next.png";
 function Use() {
   return (
@@ -9,17 +10,18 @@ function Use() {
         </h1>
 
         <div className="relative">
-          <div className="w-[700px] h-[300px] mx-auto rounded-xl bg-[#CCDCF6] flex items-center  justify-center"></div>
-          <div className="w-[730px] h-[300px] mx-auto rounded-xl bg-[#1C4F4A] flex items-center  justify-center absolute top-5 left-64 "></div>
-          <div className="w-[760px] h-[300px] mx-auto rounded-xl bg-[#FFB130] flex items-center  justify-center absolute top-10 left-60"></div>
+          <div className="w-[350px] h-[150px] md:w-[700px] md:h-[300px] mx-auto rounded-xl bg-[#CCDCF6] flex items-center  justify-center"></div>
+          <div className="w-[350px] h-[150px] md:w-[730px] md:h-[300px] mx-auto rounded-xl bg-[#1C4F4A] flex items-center  justify-center absolute top-5 md:left-64 "></div>
+          <div className="w-[350px] h-[150px] md:w-[760px] md:h-[300px] mx-auto rounded-xl bg-[#FFB130] flex items-center  justify-center absolute top-10 md:left-60"></div>
+          <div className="de"></div>
 
-          <div className="w-[800px] h-[350px] mx-auto rounded-xl bg-[#FC6719] flex items-center  justify-center absolute top-[60px] left-[220px]">
-            <div className="flex flex-col items-center justify-center px-10 py-16 gap-4 text-white text-center">
-              <img src={dest} alt="" />
+          <div className=" w-[350] h-[180px] md:w-[800px] md:h-[350px] mx-auto rounded-xl bg-[#FC6719] flex items-center  justify-center absolute top-[60px] md:left-[220px]">
+            <div className="flex flex-col items-center justify-center px-4 md:px-10 md:py-16 md:gap-4 text-white text-center">
+              <img className="w-12 md:w-fit" src={dest} alt="" />
               <h1 className="text-xl md:text-3xl font-bold">
                 Advanced Systems
               </h1>
-              <p>
+              <p className="text-xs">
                 Advanced Systems Engineering consists of solutions (and
                 respective development), increasingly relying on intelligent
                 components and subsystems to deliver improved performance for
@@ -29,9 +31,13 @@ function Use() {
           </div>
         </div>
 
-        <div className="flex items-center justify-center py-36">
+        <div className="flex flex-col items-center justify-center py-36">
           <img src={next} alt="" />
-          <div className="bg-[]"></div>
+          <div className="bg-[#1C4F4A] w-40 h-12 my-5 rounded-lg">
+            <h1 className="text-white text-center flex justify-center items-center h-full">
+              Learn More
+            </h1>
+          </div>
         </div>
       </div>
     </section>
